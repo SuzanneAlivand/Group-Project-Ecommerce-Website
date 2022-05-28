f5086f6f07810c379d75a502c82173cead57d4b1
Thilakshan Jayatharan <thilakshanj@gmail.com>
2022-05-27 21:23:39 -0400

    finished product details page

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -8,7 +8,7 @@ const ProductDetails = () => {
     useEffect(() => {
         const fetchProduct = async () => {
             //to be modified later when endpoints and handlers have been created
-            const data = await fetch(`/api/items/${itemId}`);
+            const data = await fetch(`/api/item/${itemId}`);
             const json = await data.json();
             setProduct(json.data);
         }
@@ -16,8 +16,58 @@ const ProductDetails = () => {
     },[]);    
 
     return (
-        <p>Product Details</p>
-        
+        <ProductWrapper>
+            <Image src={product.imageSrc} />
+            <InfoWrapper>
+                <Name>{product.name}</Name>
+                <Price>${product.price}</Price>
+                <BodyLocation>{product.body_location}</BodyLocation>
+                <Category>{product.category}</Category>
+                <Stock>{product.numInStock}</Stock>
+                <CompanyId>{product.companyId}</CompanyId>
+            </InfoWrapper>
+        </ProductWrapper>
     );
 }
 export default ProductDetails;
+
+const ProductWrapper = styled.div`
+    display: flex;
+    gap: 50px;    
+`;
+
+const InfoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;    
+`;
+
+const Image = styled.img`
+    width: 400px;
+    border-radius: 20px;
+`;
+
+const Name = styled.div`
+    font-size: 32px;
+    font-weight: bold;
+`;
+
+const Price = styled.div`
+    font-style: italic;
+    color: gray;
+    margin-bottom: 30px;
+`;
+
+const Category = styled.div`
+    font-size: 32px;
+    font-weight: bold;
+`;
+
+const Stock = styled.div`
+    font-size: 32px;
+    font-weight: bold;
+`;
+
+const CompanyId = styled.div`
+    font-size: 32px;
+    font-weight: bold;
+`;
