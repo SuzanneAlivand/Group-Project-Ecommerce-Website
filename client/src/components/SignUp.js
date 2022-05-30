dd8e69b82ab64ea1e36973d02a3fbceb877a57a2
Thilakshan Jayatharan <thilakshanj@gmail.com>
2022-05-30 17:01:56 -0400

    signup page basic functionality mostly complete

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,18 +1,66 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../context/Context";
+import { useHistory } from "react-router-dom";
 
 const SignUp = () => {
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [userName, setUserName] = useState(null);
+    const [signUpMessage, setSignUpMessage] = useState(null);
+
+    const {user, setUser} = useContext(UserContext); 
+  
+    const history = useHistory();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      try {
+        const data = await fetch("/api/user/register", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            },
+            body: JSON.stringify({ email: email, userName: userName, password: password }),
+        });
+            const json = await data.json();
+            setSignUpMessage(json.message);
+            console.log(json);
+            setUser(email);
+            history.push("/");
+        } catch (error) {
+            console.log("ERROR:", error.message);
+        }
+    };
+  
+    const handleEmail = (e) => {
+      setEmail(e.target.value);
+    };
+
+    const handleUserName = (e) => {
+        setUserName(e.target.value);
+      };     
+  
+    const handlePassword = (e) => {
+      setPassword(e.target.value);
+    };    
+
     return (
         <Wrapper>
-            <SignupForm>
-                <NameContainer>
-                    <FirstNameInput type="text" placeholder="First Name" required/>
-                    <LastNameInput type="text" placeholder="Last Name" required/>
-                </NameContainer>
-
+            <SignupForm onSubmit={(e) => handleSubmit(e)}>
+                <h2>Sign Up</h2>
+                <UsernameInput type="text" placeholder="Username" onChange={(e) => handleUserName(e)} required/>
+                <EmailInput type="email" placeholder="Email Address" onChange={(e) => handleEmail(e)} required/>
+                <PasswordInput type="password" placeholder="Password" onChange={(e) => handlePassword(e)} minlength="6" required/>
+                <button type="submit">Submit</button>
                 <SignUpInfo>Already a member? <Link to="/login">Login here.</Link></SignUpInfo>
+                <SignUpMessage>{signUpMessage}</SignUpMessage>
             </SignupForm>
         </Wrapper>
+        
     );
 };
 export default SignUp;
@@ -39,16 +87,26 @@ const NameContainer = styled.div`
     display: flex;
 `;
 
-const FirstNameInput = styled.input`
+const UsernameInput = styled.input`
     width: 175px;
     height: 20px;
 `;
 
-const LastNameInput = styled.input`
+const EmailInput = styled.input`
+    width: 175px;
+    height: 20px;
+`;
+
+const PasswordInput = styled.input`
     width: 175px;
     height: 20px;
 `;
 
 const SignUpInfo = styled.div`
     font-size: 14px;
+`;
+
+const SignUpMessage = styled.div`
+  font-weight: bold;
+  color: red;
 `;
